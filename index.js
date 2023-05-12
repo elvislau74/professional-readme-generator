40fa1f522fcdfbdd7f3b670d72fc1439db0ef397
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-05-11 23:15:15 -0400

    create writeReadme.js file, add all variables at the top of index that grab modules, start writeToFile function

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const writeReadme = require('writeReadme');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`, data, (err) =>
+        err ? console.error(err) : console.log("Success!")
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
